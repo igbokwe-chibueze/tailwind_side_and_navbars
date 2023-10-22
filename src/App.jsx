b1d10c76e327bf0f84ece625bd9e31aba1670963
igbokwe-chibueze <igbokwechibueze@gmail.com>
2023-10-22 23:56:06 +0100

    Cleaning.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { about, contact, home } from "./assets/icons";  // Importing various icons from a file.
 import { useState } from 'react';  // Importing the 'useState' hook from React.
 import { logo } from "./assets/images";
 
@@ -41,7 +40,7 @@ const App = () => {
                     <div className="cursor-pointer md:hidden">
                       <button type="button"
                         onClick={() => setToggle(!toggle)}  // Toggle the 'toggle' state when the icon is clicked.
-                        className="block p-1 text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none"
+                        className="block p-1 text-gray-400 rounded hover:text-white hover:bg-gray-800 hover:rounded-lg transition-all duration-300 ease-linear focus:bg-gray-800"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                           {toggle ? (
@@ -62,68 +61,6 @@ const App = () => {
                     } md:block my-2 mx-2 text-white`}
                   >
                     <Menulist/>
-                  </ul>
-                </nav>
-                {/* Card Body */}
-                <div className="bg-gray-300 md:col-span-7 h-36 md:h-auto">
-                </div>
-              </div>
-
-
-              {/* Card Two */}
-              <div className="card grid md:grid-cols-8">
-                <h4 className="mb-2 font-bold text-gray-700 md:col-span-8">Nav Two</h4>
-                {/* Card Nav */}
-                <nav className="md:col-span-1 bg-gray-900">
-                  {/* Navbar area */}
-                  <div className="p-4 flex justify-between items-center md:justify-end md:border-b-2 border-b-gray-400">
-                    <a href='#'>
-                      <img
-                        src={logo}
-                        alt='logo'
-                        className="h-8"
-                      />
-                    </a>
-                    <div className="cursor-pointer md:hidden">
-                      <button type="button"
-                        onClick={() => setToggle(!toggle)}  // Toggle the 'toggle' state when the icon is clicked.
-                        className="block p-1 text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          {toggle ? (
-                            // Render the first path(close) when toggle is true
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                          ) : (
-                            // Render the second path(Hamburger) when toggle is false
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-                          )}
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                  {/* Menu area */}
-                  <ul 
-                    className={`${
-                      !toggle ? "hidden" : "block"  // Conditionally display the menu based on 'toggle' state.
-                    } md:block my-2 mx-2 text-white`}>
-                    <li className="font-bold py-1 rounded hover:bg-gray-800">
-                      <a href="#" className="px-4 flex justify-end focus:border-r-4 border-red-500">
-                        <span>Home</span>
-                        <img className="w-5 ml-2" src={home} alt="home" />
-                      </a>
-                    </li>
-                    <li className="py-1 rounded hover:bg-gray-800">
-                      <a href="#" className="px-4 flex justify-end focus:border-r-4 border-red-500">
-                        <span>About</span>
-                        <img className="w-5 ml-2" src={about} alt="about" />
-                      </a>
-                    </li>
-                    <li className="py-1 rounded hover:bg-gray-800">
-                      <a href="#" className="px-4 flex justify-end focus:border-r-4 border-red-500">
-                        <span>Contact</span>
-                        <img className="w-5 ml-2" src={contact} alt="contact" />
-                      </a>
-                    </li>
                   </ul>
                 </nav>
                 {/* Card Body */}
