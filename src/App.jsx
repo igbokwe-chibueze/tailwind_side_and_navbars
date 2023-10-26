718f0cddeb6759aa943279e12326774ab456171f
igbokwe-chibueze <igbokwechibueze@gmail.com>
2023-10-26 07:55:26 +0100

    Created Navbar 7.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';  // Importing the 'useState' hook from React.
 import { logo } from "./assets/images";
 
-import { Menulist, Menulist2,  Menulist3, ProfileList, ProfileList2, SearchBar} from "./components"
+import { Menulist, Menulist2,  Menulist3, Menulist4, ProfileList, ProfileList2, SearchBar} from "./components"
 
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
   const [toggle4, setToggle4] = useState(false);
   const [toggle5, setToggle5] = useState(false);
   const [toggle6, setToggle6] = useState(false);
+  const [toggle7, setToggle7] = useState(false);
 
   return (
     <div>
@@ -367,14 +368,18 @@ const App = () => {
                     </div>
                     
                     {/* Search Bar(Mobile): Shows on mobile screens only */}  
-                    <div className='grow mx-3 md:hidden'>
+                    <div className='grow ml-3 mr-2 md:hidden'>
                       <SearchBar/>
                     </div>
                     
                     {/* Profile, Hamburger & Notification Btn */}
                     <div className='flex items-center space-x-2'>
-                      {/* Search Bar(Large Screens): Shows on Large screens only */}  
-                      <div className='hidden md:flex mx-3'>
+                      {/* Search Bar(Large Screens): Shows on Large screens only */}
+                      {/* I dont know why using "w-80" in conjuction with "block" 
+                        makes the searchbar longer than when using "w-full,
+                        but it makes the searchbar look better.
+                      */}
+                      <div className='hidden md:block md:w-80 mx-2'>
                         <SearchBar/>
                       </div>
                     
@@ -420,6 +425,88 @@ const App = () => {
                   >
                     <div className='px-2'>
                       <Menulist3/>
+                    </div>
+                    <div>
+                      <ProfileList2/>
+                    </div>
+                  </div>
+                </nav>
+                {/* Card Body */}
+                <div className="bg-gray-300 h-36">
+                </div>
+              </div>
+
+
+              {/* Card Seven */}
+              <div className='card grid'>
+                <h4 className="mb-2 font-bold text-gray-700">Nav Seven</h4>
+                {/* Card Nav */}
+                <nav className='bg-gray-800 '>
+                  <div className='flex justify-between items-center m-2 md:p-2'>
+                    {/* Logo */}
+                    <a href='#'>
+                        <img src={logo} alt='logo' className="h-6"/>
+                      </a>
+                    
+                    {/* Search Bar(Mobile): Shows on mobile screens only */}  
+                    <div className='grow md:grow-0 md:w-1/3 mx-3'>
+                      <SearchBar/>
+                    </div>
+                    
+                    {/* Profile, Hamburger & Notification Btn */}
+                    <div className='flex items-center space-x-2'>
+                      {/* Search Bar(Large Screens): Shows on Large screens only */}  
+                      {/* <div className='hidden md:flex mx-3'>
+                        <SearchBar/>
+                      </div> */}
+                    
+                      {/* Hamburger */}
+                      <div className='md:hidden'>
+                        <button type="button"
+                          onClick={() => setToggle7(!toggle7)}  // Toggle the 'toggle' state when the icon is clicked.
+                          className="hamburgerBtn"
+                        >
+                          {/* Hamburger svg & close svg*/}
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            {toggle7 ? (
+                              // Render the first path(close) when toggle is true
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            ) : (
+                              // Render the second path(Hamburger) when toggle is false
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                            )}
+                          </svg>
+                        </button>
+                      </div>
+                      {/* Notification Btn & Profile */}
+                      <div className='hidden md:flex items-center space-x-2'>
+                        {/* Notification Btn. 
+                            I have now moved this into "profileList" component
+                        */}
+                        {/* <button type="button" className='notificationBtn'>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                          </svg>
+                        </button> */}
+                        {/* Profile*/}
+                        <ProfileList/>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Menu area (Large Screens): Shows on Large screens only */}
+                  <div className='hidden md:flex items-center mb-2 pt-2 mx-4 border-t border-gray-700'>
+                    <Menulist4/>
+                  </div>
+
+                  {/* Menu area & Profile (Mobile): Shows on mobile screens only */}
+                  <div 
+                    className={`${
+                      !toggle7 ? "hidden" : "block"  // Conditionally display the menu based on 'toggle' state.
+                    } md:hidden`}
+                  >
+                    <div className='px-2'>
+                      <Menulist/>
                     </div>
                     <div>
                       <ProfileList2/>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Menulist from "./Menulist"
 import Menulist2 from "./Menulist2"
 import Menulist3 from "./Menulist3"
+import Menulist4 from "./Menulist4"
 
 import ProfileList from "./ProfileList"
 import ProfileList2 from "./ProfileList2"
@@ -11,6 +12,7 @@ export {
     Menulist,
     Menulist2,
     Menulist3,
+    Menulist4,
     ProfileList,
     ProfileList2,
     SearchBar
